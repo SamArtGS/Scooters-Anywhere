7aa2e2353f36aa8f1b11146d7489bdff0b80b768
Jorge Cardenas <JorgeCrdenas@hotmail.com>
2020-06-20 20:42:58 -0500

    Nuevas cargas Servicio

--- ScriptsBD/Hola.py
+++
@@ -1,6 +0,0 @@
-f = open('s-09-carga-inicial-ubicaciones.sql', 'w')
-for i in range(1, 11):
-  with open('s-09-carga-inicial-ubicaciones' + str(i) + '.sql', 'r') as file:
-    for line in file:
-      f.write(line)
-f.close()
