bfe7f3c21a3a638b63f1f9869c289e36867445ea
SamArtGS <samuelarturo@icloud.com>
2020-06-21 02:44:35 -0500

    bla

--- Web/Node/login.js
+++ Web/Node/login.js
@@ -1,3 +1,5 @@
+//VARIABLES REQUERIDAS
+
 var oracledb = require("oracledb");
 var express = require("express");
 var app = express();
@@ -8,36 +10,63 @@ var peticion = require("./responseORCL");
 var path = require('path');
 var md5 = require('md5');
 
+//MÉTODOS DE UTILIZACIÓN DE CALLBACKS
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
+app.use(express.static('public'));
 
 app.use(session({
 	secret: 'proyectoBD',
 	resave: true,
-	saveUninitialized: true
+	saveUninitialized: true,
+	cookie: { maxAge: 300000 }
 }));
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
+//ELEMENTO DE CONEXIÓN PARA CONEXIONES ASÍNCRONAS
 connection = {
       user          : "CG_PROY_ADMIN",
       password      : "samjor",
       connectString : "cursodb.c7rbflvluzyk.us-east-2.rds.amazonaws.com:1521/SCOOTERS"
 }
 
-app.use(express.static('public'));
+//GETS DE ANTES DEL INICIO DE SESIÓN
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/IniciarSesion.html'));
 });
 
+app.get('/ubicacion',function(request,response){
+	response.sendFile(path.join(__dirname+'/Ubicacion.html'))
+})
+
+app.get('/dashboard', function(request, response) {
+	session = request.session;
+
+
+	response.sendFile(path.join(__dirname + '/dashboard.html'));
+});
+
+
+app.get('/registro', function(request, response) {
+	response.sendFile(path.join(__dirname + '/Registro.html'));
+});
+
+
+app.get('/olvido', function(request, response) {
+	response.sendFile(path.join(__dirname + '/Olvido.html'));
+});
+
+
+//POST PARA INICIO DE SESIÓN
 app.post('/auth', async function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		sql = "SELECT COUNT(*) FROM USUARIO WHERE EMAIL = '"+username+"' AND PASSWORD = '"+md5(password)+"'";
+		sql = "SELECT COUNT(*) AS USUARIO FROM USUARIO WHERE EMAIL = '"+username+"' AND PASSWORD = '"+md5(password)+"'";
 		console.log(sql);
 		try {
 			connection =  await oracledb.getConnection(  {
@@ -46,9 +75,14 @@ app.post('/auth', async function(request, response) {
   			connectString : "cursodb.c7rbflvluzyk.us-east-2.rds.amazonaws.com:1521/SCOOTERS"
 		});
 			const result = await connection.execute(sql);
-			const resultado = await result.rows;
-			console.log(resultado["COUNT(*)"]);
-    		response.send(resultado["COUNT(*)"]);
+			const resultado = result.rows[0]['USUARIO'];
+			if (resultado == 1) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/dashboard');
+			}else{
+				response.redirect('/');
+			}
   		} catch (err) {
     	console.error(err);
   		} finally {
@@ -62,13 +96,9 @@ app.post('/auth', async function(request, response) {
   		}
 		response.end();
 	} else {
-		response.send('No has agregado usuario y contraseña');
+		response.redirect('/');
 		response.end();
 	}
-});
-
-app.listen(8000,function(){
-  console.log('Servidor Web - http://localhost:8000');
 });
 
 
@@ -82,6 +112,9 @@ function error(err,rs,cn){
     return 0;
 }
 
+app.listen(8000,function(){
+	console.log('Servidor Web - http://localhost:8000');
+  });
 
 //////
 /////
