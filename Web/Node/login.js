5b2350f08bda5f84a2e07e58e4f9458199c62aa7
SamArtGS <samuelarturo@icloud.com>
2020-06-20 20:38:03 -0500

    LogIn

--- Web/Node/login.js
+++ Web/Node/login.js
@@ -1,10 +1,12 @@
 var oracledb = require("oracledb");
 var express = require("express");
 var app = express();
+var session = require('express-session');
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var peticion = require("./responseORCL");
 var path = require('path');
+var md5 = require('md5');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -12,67 +14,80 @@ app.use(methodOverride());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 
-var router = express.Router();
-
-oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-
-router.get('/scooter',function(request,response){
-  var opc = parseInt(request.query.opc);
-  switch(opc){
-    case 1:
-      sql = "SELECT * FROM SCOOTER WHERE SCOOTER_ID<100";
-      peticion.open(sql,[],false,response);
-      break;
-  }
-  response.end;
-});
-
-app.use(router);
-app.listen(3000,function(){
-  console.log('Servidor Web - http://localhost:3000');
-});
-
-var app = express();
 app.use(session({
 	secret: 'proyectoBD',
 	resave: true,
 	saveUninitialized: true
 }));
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
+connection = {
+      user          : "CG_PROY_ADMIN",
+      password      : "samjor",
+      connectString : "cursodb.c7rbflvluzyk.us-east-2.rds.amazonaws.com:1521/SCOOTERS"
+}
+
+app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/IniciarSesion.html'));
 });
 
-app.post('/auth', function(request, response) {
+app.post('/auth', async function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
-				request.session.loggedin = true;
-				request.session.username = username;
-				response.redirect('/home');
-			} else {
-				response.send('Incorrect Username and/or Password!');
-			}			
-			response.end();
+		sql = "SELECT COUNT(*) FROM USUARIO WHERE EMAIL = '"+username+"' AND PASSWORD = '"+md5(password)+"'";
+		console.log(sql);
+		try {
+			connection =  await oracledb.getConnection(  {
+     		user          : "CG_PROY_ADMIN",
+      		password      : "samjor",
+  			connectString : "cursodb.c7rbflvluzyk.us-east-2.rds.amazonaws.com:1521/SCOOTERS"
 		});
+			const result = await connection.execute(sql);
+			const resultado = await result.rows;
+			console.log(resultado["COUNT(*)"]);
+    		response.send(resultado["COUNT(*)"]);
+  		} catch (err) {
+    	console.error(err);
+  		} finally {
+    	if (connection) {
+      	try {
+        	await connection.close();
+      	} catch (err) {
+        	console.error(err);
+      	}
+    	}
+  		}
+		response.end();
 	} else {
-		response.send('Please enter Username and Password!');
+		response.send('No has agregado usuario y contraseña');
 		response.end();
 	}
 });
 
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
+app.listen(8000,function(){
+  console.log('Servidor Web - http://localhost:8000');
 });
 
-app.listen(3000);
+
+function error(err,rs,cn){
+  if(err){
+    console.log(err.message);
+    if(cn!=null) close (cn);
+    return -1;
+  }
+  else
+    return 0;
+}
+
+
+//////
+/////
+////
+////
+/////
+
+
+
